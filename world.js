72b759f92a10539dc57c6dc8ca876a246224bca7
Odnt <remyalexd2003@gmail.com>
2023-11-21 00:21:24 -0500

    Exercise 5 Commit

--- world.js
+++ world.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("lookup").addEventListener("click", function () {
+        lookup("country");
+    });
+
+    document.getElementById("clookup").addEventListener("click", function () {
+        lookup("cities");
+    });
+
+    function lookup(type) {
         var countryInput = encodeURIComponent(document.getElementById("country").value);
-        var url = "world.php?country=" + countryInput;
+        var url = "world.php?country=" + countryInput + "&lookup=" + type;
 
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
@@ -9,10 +17,9 @@ document.addEventListener("DOMContentLoaded", function () {
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 document.getElementById("result").innerHTML = xhr.responseText;
-                console.log (xhr.responseText)
             }
         };
 
         xhr.send();
-    });
+    }
 });
--- world.php
+++ world.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
@@ -7,21 +7,66 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 if (isset($_GET['country'])) {
-  $country = $_GET['country'];
+    $country = $_GET['country'];
 
-  $stmt = $conn->prepare("SELECT * FROM countries WHERE name = :country");
-  $stmt->bindParam(':country', $country, PDO::PARAM_STR);
-  $stmt->execute();
+    if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities'){
+      $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE :country");
+    } else{
+      $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    }
 
-  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    $stmt->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
 
-  if ($result) {
-      echo "<h1>{$result['name']}</h1>";
-      echo "<p>Ruled by: {$result['head_of_state']}</p>";
-  } else {
-      echo "<p>Country not found</p>";
+    $stmt->execute();
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($results) {
+        echo "<table >
+        <tr>";
+
+        if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities') {
+          echo "<th>City Name</th>
+                <th>District</th>
+                <th>Population</th>";
+        }else{
+          echo "<th>Country Name</th>
+                <th>Continent</th>
+                <th>Independence Year</th>
+                <th>Head of State</th>";
+      
+        }
+        echo "</tr>";
+
+
+        foreach ($results as $row) {
+          echo "<tr>";
+
+          if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities') {
+            echo "<td>{$row['name']}</td>";
+            echo "<td>{$row['district']}</td>";
+            echo "<td>{$row['population']}</td>";
+         
+        } else {
+            
+            echo "<td>{$row['name']}</td>";
+            echo "<td>{$row['continent']}</td>";
+            echo "<td>{$row['independence_year']}</td>";
+            echo "<td>{$row['head_of_state']}</td>";
+            
+        }
+        echo "</tr>";
+
+      } 
+
+        
+
+        echo "</table>";
+    } else {
+        echo "<p>No matching countries found</p>";
+    }
   }
-} else {
-  echo "<p>No country specified</p>";
-}
 
+$conn = null;
+?>
